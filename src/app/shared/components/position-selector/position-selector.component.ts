a94da5aa487467e220a2e31a1b2f92636587fe89
kuzafkto <kuzafkto@gmail.com>
2023-12-09 11:16:57 +0100

    valueAccesor details

--- src/app/shared/components/position-selector/position-selector.component.ts
+++ src/app/shared/components/position-selector/position-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, forwardRef, Input, OnInit, Output, Renderer2 } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Position } from 'src/app/core/interfaces/player';
 
