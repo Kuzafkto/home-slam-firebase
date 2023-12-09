9c0bb60588ae7b756a6600f7989d98e30ab4dafa
kuzafkto <kuzafkto@gmail.com>
2023-12-09 10:35:01 +0100

    pipe changes

--- src/app/shared/pipes/name.pipe.ts
+++ src/app/shared/pipes/name.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'name'
 })
 export class NamePipe implements PipeTransform {
-  transform(value: string|undefined,limit:number): string {
-    if (value) {
-    let firstLetterCapital = value.charAt(0).toUpperCase() + value.slice(1);
+  transform(name: string|undefined,limit:number): string {
+    if (name) {
+    let firstLetterCapital = name.charAt(0).toUpperCase() + name.slice(1);
 
     if(firstLetterCapital.length>limit){
       return firstLetterCapital.substring(0, limit) + '... '
